87dece135be167edc4162b3a9b92ccfc97f8143c
Brandon <Soul@Brandons-MacBook-Pro.local>
2015-04-06 21:42:47 -0500

    latest changes

--- Chimera_Beta/app/src/main/java/thereal/chimera_beta/TitleViewActivity.java
+++ Chimera_Beta/app/src/main/java/thereal/chimera_beta/TitleViewActivity.java
@@ -10,16 +10,18 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.mysql.jdbc.*;
 
+
+import java.sql.*;
+
+/*
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Statement;*/
 
-import com.mysql.*;
-import java.sql.*;
+//import com.mysql.*;
 import java.util.jar.Attributes;
 
 /*
@@ -38,6 +40,11 @@ public class TitleViewActivity extends ActionBarActivity {
     public ImageView iconImg;
     public RatingBar rating;
     public Button similarBttn;
+    public Connection con;
+    public Statement stmt;
+    public ResultSet rs;
+    public String info;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,45 +58,60 @@ public class TitleViewActivity extends ActionBarActivity {
         summaryTxt.setText("blah blah blah");
         try {
             connectToAndQueryDatabase();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void connectToAndQueryDatabase() throws SQLException {
-        String username = "jstanton";
-        String password = "11471394";
-        Connection con = DriverManager.getConnection(
-                "jdbc:chimera://vrbsky-oracle.cs.ua.edu:3306/",
-                username,
-                password);
-
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM Movie WHERE Director = Stanley Kubrick");
-        String info = rs.getString(1);
-        summaryTxt.setText(info);
-        System.out.println(info);
-        Log.d("myTag", info);
-        setTitle(info);
-
-        /*
-        while (rs.next()) {
-            String info = rs.toString();
+    public void connectToAndQueryDatabase() throws IllegalAccessException, InstantiationException,
+            ClassNotFoundException, SQLException{
+        summaryTxt.setText("connecting to database");
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            String username = "jstanton";
+            String password = "11471394";
+            String url = "jdbc:mysql://vrbsky-oracle.cs.ua.edu:3306/chimera";
+            con = DriverManager.getConnection(url, username, password);
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("SELECT * FROM movie");
+            /*
+            rs.next();
+            String info = rs.getString("Name");
             summaryTxt.setText(info);
             System.out.println(info);
             Log.d("myTag", info);
-            setTitle(info);
-            /*
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-            System.out.println(rs.next());
-        }*/
+            setTitle(info);*/
+
+            while (rs.next()) {
+                info = rs.getString(1);
+                summaryTxt.setText(info);
+                System.out.println(info);
+                /*
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());
+                System.out.println(rs.next());*/
+        }
+            rs.close();
+            stmt.close();
+            con.close();
+            if(info == null)
+                summaryTxt.setText("You Failed");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
