bb71f71d734d9f602f0847d593665762c488a920
Brandon <Soul@Brandons-MacBook-Pro.local>
2015-04-06 20:20:51 -0500

    latest work + db implementation

--- Chimera_Beta/app/src/main/java/thereal/chimera_beta/TitleViewActivity.java
+++ Chimera_Beta/app/src/main/java/thereal/chimera_beta/TitleViewActivity.java
@@ -1,17 +1,76 @@
 package thereal.chimera_beta;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.mysql.jdbc.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.*;
+import java.sql.*;
+
+/*
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;*/
 
 
 public class TitleViewActivity extends ActionBarActivity {
+    public TextView summaryTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_view);
+        summaryTxt = (TextView) findViewById(R.id.titleSummary);
+        try {
+            connectToAndQueryDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void connectToAndQueryDatabase() throws SQLException {
+        String username = "jstanton";
+        String password = "11471394";
+        Connection con = DriverManager.getConnection(
+                "jdbc:chimera://vrbsky-oracle.cs.ua.edu:3306/",
+                username,
+                password);
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM Movie WHERE Director = Stanley Kubrick");
+
+        while (rs.next()) {
+            String info = rs.toString();
+            summaryTxt.setText(info);
+            System.out.println(info);
+            Log.d("myTag", info);
+            setTitle(info);
+            /*
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());
+            System.out.println(rs.next());*/
+        }
     }
 
 
